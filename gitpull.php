4ceb2cbdd270225f13d145c837ed4b79f7f660e5
Alex DiLiberto <alex@alexdiliberto.com>
2014-02-08 18:40:04 -0500

    updated gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -6,12 +6,12 @@
   try {
     $payload = json_decode($_REQUEST['payload']);
   } catch(Exception $e) {
-    file_put_contents('../logs/github.txt', $e . ' ' . $payload, FILE_APPEND);
+    file_put_contents('/logs/error.log', $e . ' ' . $payload, FILE_APPEND);
     exit(0);
   }
 
   if ($payload->ref === 'refs/heads/master') {
     $output = shell_exec('git pull');
-    file_put_contents('../logs/github.txt', $output, FILE_APPEND);
+    file_put_contents('/logs/access.log', $output, FILE_APPEND);
   }
 ?>
