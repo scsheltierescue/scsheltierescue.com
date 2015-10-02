bd41b537b33769071907b763802a911539abab04
Alex DiLiberto <alex@alexdiliberto.com>
2015-10-01 21:51:47 -0400

    Updated loading spinner

--- js/petfinder.js
+++ js/petfinder.js
@@ -1,5 +1,4 @@
 var currentOffset = 0,
-    spinner = undefined,
     isFirstGetPetsAPICall = true;
 
 /**
@@ -166,36 +165,10 @@ function normalizeToArray(prop) {
 }
 
 function spinnerStart($btnMore) {
-  var spinner_target = document.getElementById('spinner'),
-      opts = {
-        lines: 13, // The number of lines to draw
-        length: 20, // The length of each line
-        width: 10, // The line thickness
-        radius: 30, // The radius of the inner circle
-        corners: 1, // Corner roundness (0..1)
-        rotate: 0, // The rotation offset
-        direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#000', // #rgb or #rrggbb or array of colors
-        speed: 0.7, // Rounds per second
-        trail: 25, // Afterglow percentage
-        shadow: false, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: 'auto', // Top position relative to parent in px
-        left: 'auto' // Left position relative to parent in px
-      };
-
   $btnMore.addClass("invisible");
-  if(!spinner) {
-    spinner = new Spinner(opts).spin(spinner_target);
-  } else {
-    $("#spinner").show()
-    spinner.spin(spinner_target);
-  }
+  $("#spinner").show()
 }
 
 function spinnerStop() {
   $("#spinner").hide();
-  spinner.stop();
 }
