339b2983cfc9609c4904be3f14e8ffff46cbb844
Alex DiLiberto <alex@alexdiliberto.com>
2017-09-02 22:03:11 -0400

    upgrade gulpfile dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,56 @@
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var minifycss    = require('gulp-minify-css');
-var uglify       = require('gulp-uglify');
-var del          = require('del');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleancss = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const pump = require('pump');
 
 // Clean
-gulp.task('clean', function() {
+gulp.task('clean', () => {
   return del(['css', 'js/vendor']);
 });
 
 // Styles
-gulp.task('styles', ['clean'], function() {
+gulp.task('styles', ['clean'], () => {
   return gulp.src('scss/app.scss')
     .pipe(sass({
-      includePaths: ['bower_components/foundation/scss']
-    }))
+        includePaths: ['bower_components/foundation/scss']
+      })
+      .on('error', sass.logError)
+    )
     .pipe(autoprefixer({
       browsers: ['last 2 versions', 'IE >= 9'],
       cascade: false
     }))
-    .pipe(minifycss())
+    .pipe(cleancss({ debug: true }, (details) => {
+      console.log(`Original - ${details.name}: ${details.stats.originalSize}`);
+      console.log(`Minified - ${details.name}: ${details.stats.minifiedSize}`);
+    }))
     .pipe(gulp.dest('css'));
 });
 
 // Uglify
-gulp.task('uglify', ['clean'], function() {
-  return gulp.src('bower_components/modernizr/modernizr.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('js/vendor'))
+gulp.task('uglify', ['clean'], (cb) => {
+  // Pump will pipe streams together and close all of them if one of them closes
+  pump(
+    [
+      gulp.src('bower_components/modernizr/modernizr.js'),
+      uglify(),
+      gulp.dest('js/vendor')
+    ],
+    cb
+  )
 });
 
 // Default
 gulp.task('default', ['styles', 'uglify']);
 
 // Watch
-gulp.task('watch', ['clean'], function() {
+gulp.task('watch', ['clean'], () => {
   // Watch .scss files
   gulp.watch([
     'bower_components/foundation/scss/**/*.scss',
     'scss/**/*.scss'
-    ], ['styles']);
+  ], ['styles']);
 });
