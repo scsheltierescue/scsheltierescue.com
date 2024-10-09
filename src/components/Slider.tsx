a54491fd8b242f11c9341e542a91130a607a4bf5
Alex DiLiberto <alex@alexdiliberto.com>
2024-10-09 15:40:56 -0400

    starting conversion to astro

---
+++ src/components/Slider.tsx
@@ -0,0 +1,35 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import Slider from 'react-slick';
+
+export interface Item {
+  src: string;
+  width: number;
+  height: number;
+  alt?: string;
+}
+
+export default function ReactSlickSlider({ items }: { items: Item[] }) {
+  let settings = {
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    easing: 'linear',
+    fade: true,
+    focusOnSelect: true,
+    infinite: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {items.map((item) => <img {...item} />)}
+      </Slider>
+    </div>
+  );
+}
