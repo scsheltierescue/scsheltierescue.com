7fafe80206d26136ef855e66874258fd4958e5fe
Alex DiLiberto <alex@alexdiliberto.com>
2024-10-09 18:21:04 -0400

    test netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()]
+  integrations: [react(), tailwind(), icon()],
+  output: 'hybrid',
+  adapter: netlify(),
 });
