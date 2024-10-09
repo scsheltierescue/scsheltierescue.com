f56898ac43726ba827dbfc86d886e42fb81b7c59
Alex DiLiberto <alex@alexdiliberto.com>
2024-10-09 18:06:38 -0400

    Revert "test cloudflare"
    This reverts commit d7c14751e5106ece7f205d088ce6d182f5df4985.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
-  output: 'server',
-  adapter: cloudflare(),
+  integrations: [react(), tailwind(), icon()]
 });
