1ae4d818a2c67bee1eb579758196ed62df580ad1
Alex DiLiberto <alex@alexdiliberto.com>
2024-10-09 19:33:13 -0400

    Revert "test netlify"
    This reverts commit 7fafe80206d26136ef855e66874258fd4958e5fe.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
-  output: 'hybrid',
-  adapter: netlify(),
+  integrations: [react(), tailwind(), icon()]
 });
