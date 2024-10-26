35f7d7e338e28ec95dff2cb406844ad1f4e9ef69
Alex DiLiberto <alex@alexdiliberto.com>
2024-10-25 21:57:09 -0400

    passthrough image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
@@ -10,6 +10,9 @@ export default defineConfig({
   integrations: [react(), tailwind(), icon()],
   output: 'hybrid',
   adapter: cloudflare(),
+  image: {
+    service: passthroughImageService()
+  },
   vite: {
     define: {
       'process.env.PETFINDER_API_SECRET': JSON.stringify(process.env.PETFINDER_API_SECRET)
