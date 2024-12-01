84437ef1c55143aa3619663cc24df9309ad43d67
Alex DiLiberto <alex@alexdiliberto.com>
2024-11-30 22:22:35 -0500

    continued css cleanup (#31)
    * continued css cleanup
    
    * more css work

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      // boxShadow: {
+      //   'inner-strong': 'inset 0 0 3px rgba(51 51 51 / 0.5)',
+      // },
       colors: {
         'scsr-logo-c': {
           DEFAULT: 'var(--scsr-logo-c)',
@@ -16,10 +19,20 @@ export default {
         'error-c': {
           DEFAULT: 'var(--scsr-error-c)',
           darker: 'var(--scsr-error-c-darker)',
-        }
+        },
+        'selection-c': {
+          DEFAULT: 'var(--scsr-selection-c)',
+        },
       },
       fontFamily: {
         sans: ['Helvetica Neue', 'Helvetica', 'Arial', 'Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        sm: '476px', // 29.75em
+        md: '769px', // 48.063em
+        lg: '940px', // 58.75em
+        xl: '1281px', // 80.063em
+        '2xl': '1441px', //90.063em
       },
     },
   },
